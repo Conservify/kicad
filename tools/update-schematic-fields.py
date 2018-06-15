18b45694e04dbe0d0313de2504d1dcb7e11824c3
Jacob Lewallen <jlewallen@gmail.com>
2018-06-15 14:58:23 -0700

    Footprint for SPH0645 that's stencil friendly.

--- tools/update-schematic-fields.py
+++ tools/update-schematic-fields.py
@@ -133,7 +133,7 @@ def update_schematic_fields(working, filename, source):
         return False
 
     if modified:
-        kifield.insert_part_fields_into_sch(table.original, filename, True, False)
+        kifield.insert_part_fields_into_sch(table.original, filename, True, False, False)
 
     return True
 

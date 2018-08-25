7a714c24227d7ae11298824e85909b3240c46334
Jacob Lewallen <jlewallen@gmail.com>
2018-08-24 17:17:36 -0700

    Some parts changed values. Add mising things to authority.

---
+++ tools/generate-super.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xe
+
+FK=$HOME/fieldkit
+
+./kicad-tool.py --bom \
+                $FK/core/hardware/fk-core.sch \
+                $FK/atlas/hardware/fk-atlas.sch \
+                $FK/sonar/hardware/fk-sonar.sch \
+                $FK/weather/hardware/module-board/fk-weather.sch \
+                $FK/weather/hardware/sensor-board/fk-weather-sensors.sch \
+                $FK/naturalist/hardware/main-board/fk-naturalist.sch \
+                $FK/naturalist/hardware/sensor-board/fk-naturalist-sensors.sch
